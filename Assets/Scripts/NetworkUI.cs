69295e3079d0a027c7e43adad3bbafcf6d5ec033
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-06-01 19:16:00 +0100

    Updated Network to have UI

---
+++ Assets/Scripts/NetworkUI.cs
@@ -0,0 +1,23 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkUI : MonoBehaviour
+{
+    public void StartServer()
+    {
+        if (!NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
+        {
+            NetworkManager.Singleton.StartServer();
+            Debug.Log("Server started.");
+        }
+    }
+
+    public void StartClient()
+    {
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            NetworkManager.Singleton.StartClient();
+            Debug.Log("Client started.");
+        }
+    }
+}
